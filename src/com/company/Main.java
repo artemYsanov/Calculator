1dba6414a0c985170fa2b5e4fa40ae5054ef4e6e
Artem <artem.ysanov2217@gmail.com>
2022-04-04 19:35:01 +0300

    text

---
+++ src/com/company/Main.java
@@ -0,0 +1,49 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        /**
+         *@see Math.pow(num1,num2) - Метод для возведения в степень.
+         * В задаче тредуется работать с целыми числами, но метод "Math.pow" работает только с переменными double.
+         */
+    double num1;
+    double num2;
+    double equally;
+    char sign;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число:");
+        num1 = scanner.nextDouble();
+        System.out.print("Введите один из знаков операции (+, -, *, /, %, ^):");
+        sign = scanner.next().charAt(0);
+        System.out.print("Введите второе число:");
+        num2 = scanner.nextDouble();
+        switch(sign) {
+            case '+':
+                equally = num1 + num2;
+                break;
+            case '-':
+                equally = num1 - num2;
+                break;
+            case '*':
+                equally = num1 * num2;
+                break;
+            case '%':
+                equally = num1 % num2;
+                break;
+            case '^':
+                equally = Math.pow(num1,num2);
+                break;
+            case '/':
+                equally = num1 / num2;
+                break;
+            default:
+                System.out.printf("Error!");
+                return;
+        }
+        System.out.println(equally);
+    }
+
+}
